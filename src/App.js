ba1a40ad9f74d3bc87949f873b4d828a476fed4e
tora_pan <travis.pandos@gmail.com>
2021-08-17 21:11:58 -0700

    add collectionPreview component and filter in data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
+import ShopPage from "./pages/shop/ShopPage";
 
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
