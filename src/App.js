6a8949ac1fb8aec805850eaed9b11a8eea30f9e6
tora_pan <travis.pandos@gmail.com>
2021-08-19 20:15:06 -0700

    add basic functional header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
 import ShopPage from "./pages/shop/ShopPage";
-
+import Header from "./components/header/Header";
 import { Switch, Route } from "react-router-dom";
-
-
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
