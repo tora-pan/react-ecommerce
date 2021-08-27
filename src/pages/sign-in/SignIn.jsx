f79ce75d2c68aaca99744720cfd02616c445a6eb
tora_pan <travis.pandos@gmail.com>
2021-08-26 20:53:54 -0700

    add email + password login with firebase

--- src/pages/sign-in/SignIn.jsx
+++ src/pages/sign-in/SignIn.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./signIn.scss";
 import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
