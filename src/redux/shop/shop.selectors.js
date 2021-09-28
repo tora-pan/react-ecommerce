d80040bc7ff2c809cd41d7838262260ecb8832d2
tora_pan <travis.pandos@gmail.com>
2021-09-27 21:15:22 -0700

    add redux-thunk and refactor to async redux code

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -23,3 +23,8 @@ export const selectCollection = memoize((collectionUrlParam) =>
     collections ? collections[collectionUrlParam] : null
   )
 );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
